69a1fcb3164e786bf22a4b750bb070f078828f67
hafiz maaz <hafizmaaz256@gmail.com>
2026-04-28 15:23:18 +0500

    1st update 4/28/2026

--- index.js
+++ index.js
@@ -5,6 +5,31 @@ const Config = require('./src/config/config')
 const express = require('express')
 
 const app = express()
+const jsonData =
+
+{
+    "glossary": {
+        "title": "example glossary",
+        "GlossDiv": {
+            "title": "S",
+            "GlossList": {
+                "GlossEntry": {
+                    "ID": "SGML",
+                    "SortAs": "SGML",
+                    "GlossTerm": "Standard Generalized Markup Language",
+                    "Acronym": "SGML",
+                    "Abbrev": "ISO 8879:1986",
+                    "GlossDef": {
+                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
+                        "GlossSeeAlso": ["GML", "XML"]
+                    },
+                    "GlossSee": "markup"
+                }
+            }
+        }
+    }
+}
+
 
 
 app.get('/', (req, res) => {
@@ -17,6 +42,10 @@ app.get('/twitter', (req, res) => {
 
 })
 
+
+app.get("/github", (req, res) => {
+    res.json(jsonData)
+})
 
 app.listen(Config.PORT, () => {
     console.log(`Example app listening on port ${Config.PORT}`)
